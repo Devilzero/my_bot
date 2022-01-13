5f3bccccd0c11f226f3e218742d1af2af50e072f
ermaozi <admin@ermao.net>
2022-01-13 23:31:18 +0800

    修改启动脚本

--- my_bot_start.sh
+++ my_bot_start.sh
@@ -5,6 +5,5 @@ kill -9 $(ps -ef|grep start.py|grep -v grep|awk '{print $2}')
 nohup python start.py &
 ps -ef|grep start.py|grep -v grep|awk '{print $2}'
 
-if [ !-f "/etc/cron.daily/my_bot_start.sh" ];then
-    cp cd /root/my_bot/my_bot_start.sh /etc/cron.daily/
-fi
+cp /root/my_bot/my_bot_start.sh /etc/cron.daily/
+chmod +x /etc/cron.daily/my_bot_start.sh
