92338ca6fda36695be1f7580b6258a7e6be26eed
ermaozi <admin@ermao.net>
2022-01-14 20:06:38 +0800

    干掉没用的输出

--- my_bot_start.sh
+++ my_bot_start.sh
@@ -1,12 +1,12 @@
 cd /root/mcl/
 kill -9 $(ps -ef|grep mcl|grep -v grep|awk '{print $2}')
-nohup ./mcl &
+nohup ./mcl >/dev/null 2>&1 &
 
 cd /root/my_bot/
 source venv/bin/activate
 git pull
 kill -9 $(ps -ef|grep start.py|grep -v grep|awk '{print $2}')
-nohup python start.py &
+nohup python start.py >/dev/null 2>&1 &
 ps -ef|grep start.py|grep -v grep|awk '{print $2}'
 
 cp /root/my_bot/my_bot_start.sh /etc/cron.hourly/
