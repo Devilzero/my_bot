7bc2274a324416ca94674c47b46fe566e704f4fe
ermaozi <admin@ermao.net>
2022-01-14 20:58:44 +0800

    mcl重启失败

--- mcl_restart.sh
+++
@@ -1,3 +0,0 @@
-kill -9 $(ps -ef|grep mcl|grep -v grep|awk '{print $2}')
-cd /root/mcl/
-nohup ./mcl &
--- my_bot_start.sh
+++ my_bot_start.sh
@@ -3,11 +3,8 @@ kill -9 $(ps -ef|grep start.py|grep -v grep|awk '{print $2}')
 cd /root/my_bot/
 git pull
 
-source /root/my_bot/venv/bin/activate
-nohup python start.py &
-
-
 \cp /root/my_bot/my_bot_start.sh /etc/cron.hourly/
 chmod +x /etc/cron.hourly/my_bot_start.sh
-\cp /root/my_bot/mcl_restart.sh /etc/cron.hourly/
-chmod +x /etc/cron.hourly/mcl_restart.sh
+
+source /root/my_bot/venv/bin/activate
+nohup python start.py >/dev/null 2>&1 &
